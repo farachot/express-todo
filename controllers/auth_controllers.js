0143219b75d2a01982762be9b2a6d8c74a13a14d
farid <valkanid12@gmail.com>
2023-11-12 22:12:08 +0700

    auth login

---
+++ controllers/auth_controllers.js
@@ -0,0 +1,26 @@
+const User = require("../models/user_model");
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = {
+  login: async (req, res) => {
+    const userLogin = req.body;
+
+    try {
+      const user = await User.findOne({ email: userLogin.email });
+      if (!user) throw new Error("invalid user");
+      if (user.password !== userLogin.password) throw new Error("invalid user");
+
+      const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY);
+
+      res.json({
+        message: "login berhasil",
+        userId: user.id,
+        token,
+      });
+    } catch (error) {
+      res.json(error.message);
+    }
+  },
+};
